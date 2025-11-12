9a286fad2ae34416064e9d0cf8c524d63903e0c2
Kamyla Modesto <kamyla.msilva@hotmail.com>
2025-11-12 11:48:13 -0300

    Add files via upload

---
+++ pratica5/calculadora.js
@@ -0,0 +1,39 @@
+let num1 = 15;
+let num2 = 7;
+let soma = num1 + num2;
+console.log(soma); // saída: 22
+
+let contador = 0;
+contador += 10;
+contador += 5;
+console.log(contador);
+
+let a = 20;
+let b = 5;
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a/b;
+console.log (adicao, subtracao, multiplicacao, divisao);
+
+
+let num3 = 2;
+let num4 = 3;
+let e1 = num3 ** num4;
+let e2 = b ** num3;
+console.log (e1, e2);
+
+let x = 10;
+let y = 4;
+let z = 2;
+let calculo = (x + y) * z - (x / y);
+console.log(calculo);
+
+let saldo = 100;
+saldo += 50;
+console.log (saldo);
+saldo -= 30;
+console.log (saldo);
+
+
+
